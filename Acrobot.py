457198d7f622e396f57c025ac510fa4e39825f15
MohammadHossein <m.hossein.amini@gmail.com>
2020-01-01 17:05:33 +0330

    Polynomial feature extraction is added!

--- Acrobot.py
+++ Acrobot.py
@@ -4,11 +4,22 @@ from matplotlib import pyplot as plt
 
 env = gym.make('CartPole-v0')
 
-def featureExtraction(observation):
-    #  Turning obs into a 2d shape vector in numpy!!
-    features = observation.copy()
-    features = features[:, np.newaxis]
-
+def featureExtraction(observation, mode=0):
+    if not mode:  #  The features will be just observations!!!
+        #  Turning obs into a 2d shape vector in numpy!!
+        features = observation.copy()
+        features = features[:, np.newaxis]
+    elif mode == 1: #  The features are polynomials up to degree 2 of the observations!!
+        length = observation.shape[0]
+        print('obs', observation)
+        print('len', length)
+        features = np.zeros((int(length * (length + 1) / 2), 1))
+        features[0:length, :] = observation[:, np.newaxis]
+        counter = 0
+        for i in range(length):
+            for j in range(i + 1, length):
+                features[length + counter] = observation[i] * observation[j]
+                counter += 1
     #features = features[2:3, :]
     return features
 
@@ -31,11 +42,13 @@ def updateWeights(w, action, td, features, alpha = 0.2):
 actions_dict = {0: 1, 1: 0}
 landa = 0.9
 alpha = 0.001
+feature_mode = 1
 episode_no = 50
 episode_lens = []
+obs_no = 4
 #w = np.array([[5], [-5]])
 #w = np.zeros((2, 1))
-w = np.zeros((2, 4))
+w = np.zeros((2, 10))
 
 
 for i_episode in range(episode_no):
@@ -43,7 +56,7 @@ for i_episode in range(episode_no):
     action_history = []
     end_counter = 0
     observation = env.reset()
-    features = featureExtraction(observation)
+    features = featureExtraction(observation, mode=feature_mode)
     for t in range(500):
         env.render()
         #action = env.action_space.sample()
@@ -53,12 +66,12 @@ for i_episode in range(episode_no):
         action_history.append(action)
 
         if not done:
-            target = reward + landa * np.max(q(featureExtraction(observation), w))
+            target = reward + landa * np.max(q(featureExtraction(observation, mode=feature_mode), w))
             current_estimation = np.max(q_mat)
             temporal_difference = target - current_estimation
             w = updateWeights(w, action_index, temporal_difference, features, alpha)
 
-            features = featureExtraction(observation)
+            features = featureExtraction(observation, mode=feature_mode)
 
             print('angle: {}'.format(features[2, 0] * 180 / np.pi))
             print('features: {}'.format(features))
