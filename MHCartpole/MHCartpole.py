f665c5fd2474109cb40dae32b7545807ef1fb672
MohammadHossein <m.hossein.amini@gmail.com>
2021-01-02 13:59:37 +0330

    Heuristic controller for MHCartpole added

--- MHCartpole/MHCartpole.py
+++ MHCartpole/MHCartpole.py
@@ -57,14 +57,26 @@ def findEpisodeStats(hist_obs, hist_act, hist_rew):
         else:
             print(f'Actual: {ret}\t Predicted: {model_r.predict(np.array([hist_obs[i]]))}')
 
-def selectAction(obs, eps=0.1):
-    n = np.random.rand()
-    if n>eps:
-        q_l = model_l.predict(np.array([obs]))
-        q_r = model_r.predict(np.array([obs]))
-        return 0 if q_l > q_r else 1
-    else:
-        return np.random.randint(0, 2)
+def selectAction(obs, eps=0.1, policy='MonteCarlo'):
+    '''
+    In heuristic mode, whenever the angle is positive, it pushes
+    the cartpole to right, and when the angle is negative, it
+    pushes the cartpole to left.
+    '''
+    if policy.lower() in ['montecarlo', 'mc']:
+        n = np.random.rand()
+        if n>eps:
+            q_l = model_l.predict(np.array([obs]))
+            q_r = model_r.predict(np.array([obs]))
+            return 0 if q_l > q_r else 1
+        else:
+            return np.random.randint(0, 2)
+    elif policy.lower() in ['hand-coded', 'manual', 'heuristic']:
+        if obs[2] > 0:
+            return 1
+        else:
+            return 0
+
 
 hist_obs = []
 hist_act = []
@@ -77,11 +89,11 @@ for i_episode in range(200):
 
     observation = env.reset()
     hist_obs.append(observation)
-    for t in range(100):
+    for t in range(200):
         env.render()
         # print(observation)
         # action = env.action_space.sample()
-        action = selectAction(observation, 0.2)
+        action = selectAction(observation, 0.2, 'heuristic')
         hist_act.append(action)
         observation, reward, done, info = env.step(action)
         hist_rew.append(reward)
@@ -89,7 +101,7 @@ for i_episode in range(200):
         sleep(0.02)
         if done:
             print("Episode finished after {} timesteps".format(t+1))
-            findEpisodeStats(hist_obs, hist_act, hist_rew)
+            # findEpisodeStats(hist_obs, hist_act, hist_rew)
             # input()
             break
 
