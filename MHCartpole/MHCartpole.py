8bf64b858ee7b10c606f39eb437426eaa0e16c03
MohammadHossein <m.hossein.amini@gmail.com>
2021-01-09 13:42:20 +0330

    MonteCarlo with neural net done!

--- MHCartpole/MHCartpole.py
+++ MHCartpole/MHCartpole.py
@@ -9,34 +9,59 @@ import matplotlib.pyplot as plt
 
 env = gym.make('CartPole-v0')
 
-model_l = Sequential([
-    Dense(4, 'sigmoid', input_shape=(4,)),
+def saveModel():
+    model.save('model')
+
+def loadModel():
+    model = keras.models.load_model('model')
+    return model
+
+model = loadModel()
+
+# model = Sequential([
+#     Dense(4, 'tanh', input_shape=(4,)),
+#     Dense(2)
+# ])
+
+model_train = Sequential([
+    model,
     Dense(1)
 ])
 
-model_r = Sequential([
-    Dense(4, 'sigmoid', input_shape=(4,)),
-    Dense(1)
-])
+model_train.compile(optimizer=Adam(1.), loss='mse')
 
-model_l.compile(optimizer=Adam(1.), loss='mse')
-model_r.compile(optimizer=Adam(1.), loss='mse')
+def processModel(model, i):
+    w = np.zeros((2, 1))
+    w[i, 0] = 1
+    model.layers[-1].set_weights([w, np.array([0])])
+    model.layers[-1].trainable = False
+
+
+# print(model_train.layers[-1].get_weights())
+# print(model_train.layers[-1].trainable)
+# processModel(model_train, 1)
+# print(model_train.layers[-1].get_weights())
+# print(model_train.layers[-1].trainable)
+# print(model(np.array([[4, 2, 1., 0.5]])))
+# print(model_train(np.array([[4, 2, 1., 0.5]])))
+
 
 def findEpisodeStats(hist_obs, hist_act, hist_rew):
     print('Before Training: ')
-
     X_batch_l, y_batch_l = [], []
     X_batch_r, y_batch_r = [], []
     for i in range(len(hist_act)):
         ret = sum(hist_rew[i:])
+        # if len(hist_act) >= 199 and i > 100:
+        #     ret = 200
         if hist_act[i]==0:
             X_batch_l.append(hist_obs[i])
             y_batch_l.append(ret)
-            print(f'Actual: {ret}\t Predicted: {model_l.predict(np.array([hist_obs[i]]))}')
+            print(f'Actual: {ret}\t Predicted: {model.predict(np.array([hist_obs[i]]))}')
         else:
             X_batch_r.append(hist_obs[i])
             y_batch_r.append(ret)
-            print(f'Actual: {ret}\t Predicted: {model_r.predict(np.array([hist_obs[i]]))}')
+            print(f'Actual: {ret}\t Predicted: {model.predict(np.array([hist_obs[i]]))}')
 
     X_batch_l = np.array(X_batch_l)
     X_batch_r = np.array(X_batch_r)
@@ -44,20 +69,23 @@ def findEpisodeStats(hist_obs, hist_act, hist_rew):
     y_batch_r = np.array(y_batch_r)
     print(len(X_batch_l), len(y_batch_l), len(X_batch_r), len(y_batch_r))
     print(X_batch_l.shape, X_batch_r.shape)
-    for i in range(5):
+    for i in range(2):
         if len(X_batch_l):
-            model_l.train_on_batch(X_batch_l, y_batch_l)
+            processModel(model_train, 0)
+            model_train.train_on_batch(X_batch_l, y_batch_l)
         if len(X_batch_r):
-            model_r.train_on_batch(X_batch_r, y_batch_r)
-    
+            processModel(model_train, 1)
+            model_train.train_on_batch(X_batch_r, y_batch_r)
+            
+
     print('After Training: ')
 
     for i in range(len(hist_act)):
         ret = sum(hist_rew[i:])
         if hist_act[i]==0:
-            print(f'Actual: {ret}\t Predicted: {model_l.predict(np.array([hist_obs[i]]))}')
+            print(f'Actual: {ret}\t Predicted: {model.predict(np.array([hist_obs[i]]))}')
         else:
-            print(f'Actual: {ret}\t Predicted: {model_r.predict(np.array([hist_obs[i]]))}')
+            print(f'Actual: {ret}\t Predicted: {model.predict(np.array([hist_obs[i]]))}')
 
 def selectAction(obs, eps=0.1, policy='MonteCarlo'):
     '''
@@ -68,9 +96,8 @@ def selectAction(obs, eps=0.1, policy='MonteCarlo'):
     if policy.lower() in ['montecarlo', 'mc']:
         n = np.random.rand()
         if n>eps:
-            q_l = model_l.predict(np.array([obs]))
-            q_r = model_r.predict(np.array([obs]))
-            return 0 if q_l > q_r else 1
+            q = model.predict(np.array([obs]))
+            return 0 if q[0, 0] > q[0, 1] else 1
         else:
             return np.random.randint(0, 2)
     elif policy.lower() in ['hand-coded', 'manual', 'heuristic']:
@@ -79,16 +106,6 @@ def selectAction(obs, eps=0.1, policy='MonteCarlo'):
         else:
             return 0
 
-def saveModels():
-    model_l.save('model_l')
-    model_r.save('model_r')
-
-def loadModels():
-    model_l = keras.models.load_model('model_l')
-    model_r = keras.models.load_model('model_r')
-    return model_l, model_r
-
-model_l, model_r = loadModels()
 eps = 0.1
 
 
@@ -119,7 +136,7 @@ for i_episode in range(50):
             findEpisodeStats(hist_obs, hist_act, hist_rew)
             episode_lens.append(t)            
             if not (i_episode % 10):
-                saveModels()
+                saveModel()
             break
 
 print(episode_lens)
