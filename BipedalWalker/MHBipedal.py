f4a24ccdd98db79edf0f1825401652f0dda2c1e5
M-H-Amini <42590433+M-H-Amini@users.noreply.github.com>
2021-05-13 21:06:59 +0430

    Simple walk...

--- BipedalWalker/MHBipedal.py
+++ BipedalWalker/MHBipedal.py
@@ -1,11 +1,22 @@
 import gym
+import numpy as np 
 
 env = gym.make('BipedalWalker-v3')
-env.reset()
+a0, a1, b0, b1 = 0, 0, 0, 0
 
-for _ in range(1000):
-    env.render()
-    print(env.action_space.sample())
-    env.step(env.action_space.sample()) # take a random action
-
+for i_episode in range(20):
+    observation = env.reset()
+    for t in range(500):
+        env.render()
+        # print(observation)
+        a0 = np.sin(2*np.pi*t/10)
+        a1 = -a0
+        b0 = -1
+        b1 = 1
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(np.array([a0, a1, b0, b1]))
+        print(reward)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
 env.close()
